e017efaa873f16d3ae11cc3e67ef5c71e25aa4e5
Hubery <hubery8065@hotmail.com>
2026-05-25 17:32:07 +0800

    feat:添加备案号

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -14,6 +14,20 @@ function MainLayout() {
         {/* Outlet 表示这里渲染子路由页面 */}
         <Outlet />
       </main>
+
+      {/* 页脚：放备案号 */}
+      <footer className="site-footer">
+        <div className="site-footer-inner">
+          <a
+            href="https://beian.miit.gov.cn/"
+            target="_blank"
+            rel="noreferrer"
+            className="beian-link"
+          >
+            蜀ICP备2026026434号
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
