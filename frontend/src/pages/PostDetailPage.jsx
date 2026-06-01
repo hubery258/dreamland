f8db38ed4b0b59f558086b895aa9d2f5fff4c62c
Hubery <hubery8065@hotmail.com>
2026-06-01 15:09:35 +0800

    feat: add giscus comment section

--- frontend/src/pages/PostDetailPage.jsx
+++ frontend/src/pages/PostDetailPage.jsx
@@ -1,5 +1,6 @@
 // src/pages/PostDetailPage.jsx
 
+import Giscus from "@giscus/react";
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
@@ -22,6 +23,7 @@ function PostDetailPage() {
   // 从 URL 中获取 slug，例如 /posts/我的第一篇文章
   const { slug } = useParams();
 
+  // state管理
   const [post, setPost] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
@@ -32,6 +34,7 @@ function PostDetailPage() {
         setLoading(true);
         setError("");
 
+        // 获取后端数据的函数保存在client.js中
         const data = await getPostBySlug(slug);
         setPost(data);
       } catch (err) {
@@ -93,6 +96,23 @@ function PostDetailPage() {
         <article className="markdown-body">
           <ReactMarkdown>{post.content}</ReactMarkdown>
         </article>
+        <div className="post-comments-section">
+          <Giscus 
+            id="comment"
+            repo="hubery258/dreamland"
+            repoId="R_kgDOSaFIOw"
+            category="comment"
+            categoryId="DIC_kwDOSaFIO84C-Qb7"
+            mapping="pathname"
+            strict="0"
+            reactionsEnabled="1"
+            emitMetadata="1"
+            inputPosition="top"
+            theme="light"
+            lang="zh-CN"
+            loading="lazy"
+          />
+        </div>
       </div>
     </section>
   );
